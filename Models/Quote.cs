09450d3739433855ef09798ebee7bfaa4516cf43
wontinny <brandon1won@gmail.com>
2023-03-24 03:29:45 -0500

    date option in quote now starts at current date

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -1,30 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace qenergy.Models
+public class Quote
 {
-    public class Quote
-    {
-        [Key]
-        public int Id { get; set; } // Unique identifier and primary key for database
+    public int Id { get; set; }
 
-        [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "Please enter a valid number")]
-        public int GallonsRequested { get; set; }
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Please enter a valid number")]
+    public int GallonsRequested { get; set; }
 
-        public string? DeliveryAddress { get; set; }
+    public string DeliveryAddress { get; set; }
 
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime DeliveryDate { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public Nullable<DateTime> DeliveryDate { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal SuggestedPricePerGallon { get; set; }
+    [DisplayFormat(DataFormatString = "{0:C}")]
+    public decimal SuggestedPricePerGallon { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal TotalAmountDue { get; set; }
-
-        [Required]
-        public int customerId { get; set; } // identifier (user id) of who made the quote
-    }
+    [DisplayFormat(DataFormatString = "{0:C}")]
+    public decimal TotalAmountDue { get; set; }
 }
-
