22aa3d3cb187728f7e10a6b27342f2d109f39f93
Andres F <andres06037@gmail.com>
2023-04-01 17:19:01 -0500

    updated encryption to use hashing

--- Services/PasswordEncryption.cs
+++ Services/PasswordEncryption.cs
@@ -1,4 +1,7 @@
-﻿namespace qenergy.Services
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace qenergy.Services
 {
     public class PasswordEncryption
     {
@@ -11,6 +14,17 @@
         {
             var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+        public static string getHash(string text)
+        {
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
         }
     }
 }
