b60c91870533f84382022ad7523e621704de9b0f
Andres F <andres06037@gmail.com>
2023-04-01 01:01:08 -0500

    Added password encryption and decryption functions

---
+++ Services/PasswordEncryption.cs
@@ -0,0 +1,16 @@
+﻿namespace qenergy.Services
+{
+    public class PasswordEncryption
+    {
+        public static string EncryptPasswordBase64(string pass)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(pass); 
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string DecryptPasswordBase64(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
