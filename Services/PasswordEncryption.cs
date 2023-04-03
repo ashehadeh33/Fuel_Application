3342e1a008275dbb7591a8c49468e5d7a32b6acb
Andres F <andres06037@gmail.com>
2023-04-02 22:57:09 -0500

    Deleted previous password encryption

--- Services/PasswordEncryption.cs
+++ Services/PasswordEncryption.cs
@@ -5,16 +5,6 @@ namespace qenergy.Services
 {
     public class PasswordEncryption
     {
-        public static string EncryptPasswordBase64(string pass)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(pass); 
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-        public static string DecryptPasswordBase64(string base64EncodedData)
-        {
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-        }
         public static string getHash(string text)
         {
             // SHA512 is disposable by inheritance.  
