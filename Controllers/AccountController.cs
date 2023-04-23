64f41d82a402128fe5a91ea6c1f5593d1cade6c1
Andres F <andres06037@gmail.com>
2023-04-23 16:54:15 -0500

    Added EditProfile GET and POST, will autofill values as needed and change profile information

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -96,10 +96,108 @@ namespace qenergy.Controllers
             return View(user.profile);
         }
 
+        [HttpGet]
         public ActionResult EditProfile()
         {
-            Profile profile = new Profile();
+
+            int userId = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            User user = _service.GetUserById(userId);
+
+            if (user == null)
+            {
+                // user not logged in
+                return RedirectToAction("Login", "Account");
+            }
+
+            Profile profile = user.profile;
+            profile.States = new List<SelectListItem>
+            {
+                new SelectListItem() {Text="Alabama", Value="AL"},
+                new SelectListItem() { Text="Alaska", Value="AK"},
+                new SelectListItem() { Text="Arizona", Value="AZ"},
+                new SelectListItem() { Text="Arkansas", Value="AR"},
+                new SelectListItem() { Text="California", Value="CA"},
+                new SelectListItem() { Text="Colorado", Value="CO"},
+                new SelectListItem() { Text="Connecticut", Value="CT"},
+                new SelectListItem() { Text="District of Columbia", Value="DC"},
+                new SelectListItem() { Text="Delaware", Value="DE"},
+                new SelectListItem() { Text="Florida", Value="FL"},
+                new SelectListItem() { Text="Georgia", Value="GA"},
+                new SelectListItem() { Text="Hawaii", Value="HI"},
+                new SelectListItem() { Text="Idaho", Value="ID"},
+                new SelectListItem() { Text="Illinois", Value="IL"},
+                new SelectListItem() { Text="Indiana", Value="IN"},
+                new SelectListItem() { Text="Iowa", Value="IA"},
+                new SelectListItem() { Text="Kansas", Value="KS"},
+                new SelectListItem() { Text="Kentucky", Value="KY"},
+                new SelectListItem() { Text="Louisiana", Value="LA"},
+                new SelectListItem() { Text="Maine", Value="ME"},
+                new SelectListItem() { Text="Maryland", Value="MD"},
+                new SelectListItem() { Text="Massachusetts", Value="MA"},
+                new SelectListItem() { Text="Michigan", Value="MI"},
+                new SelectListItem() { Text="Minnesota", Value="MN"},
+                new SelectListItem() { Text="Mississippi", Value="MS"},
+                new SelectListItem() { Text="Missouri", Value="MO"},
+                new SelectListItem() { Text="Montana", Value="MT"},
+                new SelectListItem() { Text="Nebraska", Value="NE"},
+                new SelectListItem() { Text="Nevada", Value="NV"},
+                new SelectListItem() { Text="New Hampshire", Value="NH"},
+                new SelectListItem() { Text="New Jersey", Value="NJ"},
+                new SelectListItem() { Text="New Mexico", Value="NM"},
+                new SelectListItem() { Text="New York", Value="NY"},
+                new SelectListItem() { Text="North Carolina", Value="NC"},
+                new SelectListItem() { Text="North Dakota", Value="ND"},
+                new SelectListItem() { Text="Ohio", Value="OH"},
+                new SelectListItem() { Text="Oklahoma", Value="OK"},
+                new SelectListItem() { Text="Oregon", Value="OR"},
+                new SelectListItem() { Text="Pennsylvania", Value="PA"},
+                new SelectListItem() { Text="Rhode Island", Value="RI"},
+                new SelectListItem() { Text="South Carolina", Value="SC"},
+                new SelectListItem() { Text="South Dakota", Value="SD"},
+                new SelectListItem() { Text="Tennessee", Value="TN"},
+                new SelectListItem() { Text="Texas", Value="TX"},
+                new SelectListItem() { Text="Utah", Value="UT"},
+                new SelectListItem() { Text="Vermont", Value="VT"},
+                new SelectListItem() { Text="Virginia", Value="VA"},
+                new SelectListItem() { Text="Washington", Value="WA"},
+                new SelectListItem() { Text="West Virginia", Value="WV"},
+                new SelectListItem() { Text="Wisconsin", Value="WI"},
+                new SelectListItem() { Text="Wyoming", Value="WY"}
+            };
             return View(profile);
+        }
+
+        [HttpPost]
+        public ActionResult EditProfile(Profile profile)
+        {
+
+            int userId = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            User user = _service.GetUserById(userId);
+
+            if (user == null)
+            {
+                // user not logged in
+                return RedirectToAction("Login", "Account");
+            }
+
+            ModelState["States"].ValidationState = ModelValidationState.Valid;
+            ModelState["user"].ValidationState = ModelValidationState.Valid;
+            if (ModelState.IsValid)
+            {
+                // Delete current profile and remove from user
+                int profileIdToDelete = user.profile.Id;
+                user.profile = null;
+                _service.DeleteProfileById(profileIdToDelete);
+
+                // bindProfileToUser
+                // Save to database or other processing here
+                _service.bindProfileToUser(profile, userId);
+                
+                return RedirectToAction("QuoteHistory", "Quote");
+            }
+
+
+            return RedirectToAction("EditProfile", "Account");
         }
 
         [HttpGet]
