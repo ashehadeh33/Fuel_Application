182fe8acebd743897074bc0b4b53d90a68a36b69
Andres F <andres06037@gmail.com>
2023-04-23 20:10:09 -0500

    Pricing service module

---
+++ Services/PricingService.cs
@@ -0,0 +1,32 @@
+﻿using qenergy.Data;
+using qenergy.Models;
+
+namespace qenergy.Services
+{
+    public class PricingService
+    {
+        private readonly AccountService _service;
+        public PricingService(AccountService service) // inject AccountService
+        {
+            _service = service;
+        }
+
+        public bool hasQuoteHistory(User user)
+        {
+            if (_service.GetAllQuotesByUser(user.Id).Count() > 0) // has quotes
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public bool inTexas(User user)
+        {
+            if (user.profile.State == "TX")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
